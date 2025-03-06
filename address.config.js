6f26a1e4ce4f43254e688e55d8820dbb6c0bde72
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2025-03-06 15:50:11 +0100

    chore: restrict whitelisted addresses (#21)

--- address.config.js
+++ address.config.js
@@ -2,7 +2,13 @@ const PONTUSX_ADDRESSES = require('./pontusxAddresses.json')
 
 module.exports = {
   whitelists: {
-    'nft.owner': [],
+    'nft.owner': [
+      '0xe065d46af7984e3350a54a4eb8c7b9fa6f23188a', // neusta aerospace GmbH
+      '0x7c73f0c152a81e2f75d301e848e59ca5711e92a5', // Radiusmedia KG
+      '0x20d5f579f9b4a070c14cc9aab4663500f3b94022', // Airbus Defense and Space GmbH
+      '0x83253572e146ad1b66e631280bc573f5c39c0163', // neusta aerospace GmbH
+      '0x55a569079e3ee9c1f13a7b29d77bd08025f884e6' // deltaDAO AG
+    ],
     'datatokens.address': []
   },
   featured: [],
